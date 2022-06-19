1f43013d2d956cca074508fcc52255181dcabb31
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-06-20 03:01:43 +0600

    temparature layout done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [temp, setTemp] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <div className="container">
+      <div className="tem">
+          <h2>Temparature Control App</h2>
+          <div>
+            <div className='degree'>
+              <h2>{ temp} </h2>
+            </div>
+            <div className='btn'>
+              <button className='increment'>+</button>
+              <button className='decrement'>-</button>
+            </div>
+          </div>
+        </div>
+        </div>
     </div>
   );
 }
